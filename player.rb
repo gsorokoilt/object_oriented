c15f73fe03ffad7b8ad7368d9b998788286348b4
GORDON SOROKOLIT <gordon.sorokolit@gmail.com>
2018-07-06 19:14:00 -0400

    update player class

--- player.rb
+++ player.rb
@@ -6,8 +6,43 @@ class Player
     @lives = 5
   end
 
+  def restart
+    @gold_coins = 0
+    @health_points += 10
+    @lives += 5
+  end
+
+  def level_up
+    @lives += 1
+  end
+
+  def do_battle(damage)
+    @health_points = @health_points - damage
+
+    if @health_points == 0 && @lives == 0
+      restart
+    end
+
+    if @health_points == 0
+        @lives -= 1
+        @health_points = 10
+    end
+
+  end
+
+  def collect_treasure
+    @gold_coins += 1
+    if @gold_coins % 10 == 0
+      level_up
+    end
+  end
+
 end
 
 gordon = Player.new
+
+5.times do gordon.do_battle(10)
+end
+
 
 p gordon
